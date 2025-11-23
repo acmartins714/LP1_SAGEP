aa1652e405600b83b7995c42150eeb0eec90bb04
acmartins714 <acmartins714@gmail.com>
2025-11-23 11:33:34 -0300

    Adição dos controllers Pastoral e Pessoa

---
+++ SAGEP/src/controller/PastoralController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import model.Pastoral;
+import model.Pessoa;
+import service.PastoralService;
+
+import java.util.List;
+
+public class PastoralController {
+
+    private PastoralService pastoralService = new PastoralService();
+
+    public void cadastrarpastoral(Pastoral pastoral) {
+        pastoralService.cadastrarPastoral(pastoral);
+    }
+
+    public List<Pastoral> listarPastorais() {
+        return pastoralService.listarPastorais();
+    }
+
+    public void atualizarPastoralNome(int id, String novoNome) {
+        pastoralService.atualizarPastoralNome(id, novoNome);
+    }
+
+    public void atualizarPastotalResponsavel(int id, Pessoa novoResponsavel) {
+        pastoralService.atualizarPastoralResponsavel(id, novoResponsavel);
+    }
+
+    public void atualizarPastoralEmail(int id, String novoEmail) {
+        pastoralService.atualizarPastoralEmail(id, novoEmail);
+    }
+
+    public void atualizarPastoralTelefone(int id, String novoTelefone) {
+        pastoralService.atualizarPastoralTelefone(id, novoTelefone);
+    }
+
+}
---
+++ SAGEP/src/controller/PessoaController.java
@@ -0,0 +1,77 @@
+package controller;
+
+import model.Endereco;
+import model.Paroquia;
+import model.Pessoa;
+import service.PessoaService;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PessoaController {
+
+    private PessoaService pessoaService = new PessoaService();
+
+    //Metods
+    public void cadastrarPess(Pessoa pessoa) {
+        pessoaService.cadastrarPessoa(pessoa);
+    }
+
+    public List<Pessoa> listarPessoas() {
+        return pessoaService.listarPessoas();
+    }
+
+    public void atualizarPessoaNome(int id, String novoNome) {
+        pessoaService.atualizarPessoaNome(id, novoNome);
+    }
+
+    public void atualizarPessoaCpf(int id, String novoCpf) {
+        pessoaService.atualizarPessoaCpf(id, novoCpf);
+    }
+
+    public void atualizarPessoaDataNascimento(int id, LocalDate novaDataNascimento) {
+        pessoaService.atualizarPessoaDataNascimento(id, novaDataNascimento);
+    }
+
+    public void atualizarPessoaNumeroCertNasc(int id, String novoNumeroCertNasc) {
+        pessoaService.atualizarPessoaNumeroCertNasc(id, novoNumeroCertNasc);
+    }
+
+    public void atualizarPessoaCidadeNasc(int id, String novaCidadeNasc) {
+        pessoaService.atualizarPessoaCidadeNasc(id, novaCidadeNasc);
+    }
+
+    public void atualizarPessoaUfNasc(int id, String novaUfNasc) {
+        pessoaService.atualizarPessoaUfNasc(id, novaUfNasc);
+    }
+
+    public void atualizarPessoaEndereco(int id, Endereco novoEndereco) {
+        pessoaService.atualizarPessoaEndereco(id, novoEndereco);
+    }
+
+    public void atualizarPessoaPaisCasadosIgreja(int id, boolean novoPaisCasadosIgreja) {
+        pessoaService.atualizarPessoaPaisCasadosIgreja(id, novoPaisCasadosIgreja);
+    }
+
+    public void atualizarPessoaParoquiaCasamento(int id, Paroquia novaParoquiaCasamento) {
+        pessoaService.atualizarPessoaParoquiaCasamento(id, novaParoquiaCasamento);
+    }
+
+    public void atualizarPessoaCasamentoCivil(int id, boolean novoCasamentoCivil) {
+        pessoaService.atualizarPessoaCasamentoCivil(id, novoCasamentoCivil);
+    }
+
+    public void atualizarPessoaTelefone(int id, String novoTelefone) {
+        pessoaService.atualizarPessoaTelefone(id, novoTelefone);
+    }
+
+    public void atualizarPessoaEmail(int id, String novoEmail) {
+        pessoaService.atualizarPessoaEmail(id, novoEmail);
+    }
+
+    public void deletarPessoa(int id) {
+        pessoaService.deletarPessoa(id);
+    }
+
+}
